877439808cee27a80eefec572887ae937a1c9bbe
Corey Hayden <cghayden@gmail.com>
2018-09-13 13:56:51 -0400

    added hydrateDancerData

--- src/scripts/helpers.js
+++ src/scripts/helpers.js
@@ -7,10 +7,32 @@ export const loadUserData = async (uid, usertype) => {
     const docRef = await firestore.collection(usertype).doc(uid);
     return await docRef
       .get() //returns a DocumentSnapshot
-      .then(doc => (doc.exists ? doc.data() : "No such document!"));
+      .then(
+        doc =>
+          doc.exists ? hydrateDancerData(doc.data()) : "No such document!"
+      );
+    // .then(user => {
+    //   user.dancers = hydrateDancerData(user);
+
+    //   console.log("user", user);
+    //   return user;
+    // });
+    // .then(user => user);
   } catch (error) {
     return error;
   }
+};
+
+export const hydrateDancerData = async user => {
+  const dancers = user.dancers;
+  const dancerData = [];
+  for (const docRef of dancers) {
+    let dancer = await docRef.get().then(res => res.data());
+    dancerData.push(dancer);
+  }
+  user.dancers = dancerData;
+  console.log("new user", user);
+  return user;
 };
 
 export const firestoreTests = async (uid, usertype) => {
